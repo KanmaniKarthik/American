ab0887a45e75bbd9b60baadc4e9eaa071a70211f
KanmaniKarthik <kanmaniihere@gmail.com>
2024-06-10 11:28:39 +0530

    Indian commits it

--- src/ClsIndian.java
+++ src/ClsIndian.java
@@ -5,6 +5,7 @@ public class ClsIndian
 	public static void main(String[] args)
 	{
 
+		System.out.println("Hello!");
 		System.out.println("Created a class by Kanmani");
 		System.out.println("Learning to push into repository");
 	}
